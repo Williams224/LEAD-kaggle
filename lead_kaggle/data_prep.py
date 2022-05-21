59706742a9d685591f58f0190bba1c21fd5084f2
Tim Williams <timothy.williams@thetrainline.com>
2022-05-21 20:11:42 +0100

    getting going with the modelling, still some tracking to put in place

--- lead_kaggle/data_prep.py
+++ lead_kaggle/data_prep.py
@@ -1,23 +1,60 @@
 from cmath import pi
 import pandas as pd
 
+
 def pipeline(df):
     """
     should take a dataframe, clean + add features and then write to parquet.  Should apply to both train and test.
-    
-    """
-    df = df.dropna(subset="meter_reading")
 
-    df['24h_avg'] = df.sort_values(by = ['timestamp'], ascending = True).groupby('building_id')['meter_reading'].rolling(24,24).mean().reset_index(drop = True, level = 0)
-    df['24h_std'] = df.sort_values(by = ['timestamp'], ascending = True).groupby('building_id')['meter_reading'].rolling(24,24).std().reset_index(drop = True, level = 0)
+    """
+    # df = df.dropna(subset="meter_reading")
+
+    df["24h_avg"] = (
+        df.sort_values(by=["timestamp"], ascending=True)
+        .groupby("building_id")["meter_reading"]
+        .rolling(24, 3)
+        .mean()
+        .reset_index(drop=True, level=0)
+    )
+    df["24h_std"] = (
+        df.sort_values(by=["timestamp"], ascending=True)
+        .groupby("building_id")["meter_reading"]
+        .rolling(24, 3)
+        .std()
+        .reset_index(drop=True, level=0)
+    )
+    df["1w_avg"] = (
+        df.sort_values(by=["timestamp"], ascending=True)
+        .groupby("building_id")["meter_reading"]
+        .rolling(168, 24)
+        .mean()
+        .reset_index(drop=True, level=0)
+    )
+    df["1w_std"] = (
+        df.sort_values(by=["timestamp"], ascending=True)
+        .groupby("building_id")["meter_reading"]
+        .rolling(168, 24)
+        .std()
+        .reset_index(drop=True, level=0)
+    )
+    df["minus_24h_val"] = (
+        df.sort_values(by=["timestamp"], ascending=True)
+        .groupby("building_id")["meter_reading"]
+        .shift(24, fill_value=0)
+    )
+    df["minus_week_val"] = (
+        df.sort_values(by=["timestamp"], ascending=True)
+        .groupby("building_id")["meter_reading"]
+        .shift(168, fill_value=0)
+    )
 
     def z_score(row):
-        if row['24h_std'] > 0.0:
-            return (row['meter_reading'] - row['24h_avg'])/row['24h_std']
+        if row["24h_std"] > 0.0:
+            return (row["meter_reading"] - row["24h_avg"]) / row["24h_std"]
         else:
             return 0.0
 
-    df['z_score'] = df.apply(z_score, axis=1)
+    df["z_score"] = df.apply(z_score, axis=1)
     df = df.fillna(0)
 
     return df
@@ -25,10 +62,16 @@ def pipeline(df):
 
 if __name__ == "__main__":
 
+    df_train = pd.read_csv(
+        "/Users/TimothyW/Fun/energy-kaggle/data/train_features.csv",
+        parse_dates=["timestamp"],
+    )
+
+    pipeline(df_train).to_parquet("data/prepped_train_features.parquet")
+
     df_test = pd.read_csv(
         "/Users/TimothyW/Fun/energy-kaggle/data/test_features.csv",
         parse_dates=["timestamp"],
     )
 
-    
     pipeline(df_test).to_parquet("data/prepped_test_features.parquet")
--- lead_kaggle/modelling.py
+++ lead_kaggle/modelling.py
@@ -1,73 +1,200 @@
+from ast import Import
 from venv import create
+import matplotlib
 import pandas as pd
 from sklearn.model_selection import GroupKFold
 from sklearn.metrics import RocCurveDisplay
-from catboost import CatBoostClassifier, Pool
+from catboost import (
+    CatBoostClassifier,
+    Pool,
+    EShapCalcType,
+    EFeaturesSelectionAlgorithm,
+)
 from sklearn.metrics import roc_auc_score
 import matplotlib.pyplot as plt
+import shap
+import numpy as np
+import ipywidgets
+
+PLOTS = {}
+
+try:
+    from imblearn.over_sampling import SMOTE
+except ImportError:
+    from imblearn.over_sampling import SMOTE
+
 
 def create_submission(clf, test_set_features, f):
     X = test_set_features[f]
     print(X.info())
-    preds = clf.predict(X.values)
-    df_submit = test_set_features[['row_id']]
-    df_submit['anomaly'] = preds
+    preds = clf.predict(X)
+    df_submit = test_set_features[["row_id"]]
+    df_submit["anomaly"] = preds
     return df_submit
+
+
+def compare_train_test(clf, X_train, y_train, X_test, y_test, bins=30):
+    fig = plt.figure()
+    decisions = []
+    for X, y in ((X_train, y_train), (X_test, y_test)):
+        d1 = clf.predict_proba(
+            X[
+                (y > 0.5).reshape(
+                    y.shape[0],
+                )
+            ]
+        )[:, 1]
+        d2 = clf.predict_proba(
+            X[
+                (y < 0.5).reshape(
+                    y.shape[0],
+                )
+            ]
+        )[:, 1]
+        decisions += [d1, d2]
+
+    low = min(np.min(d) for d in decisions)
+    high = max(np.max(d) for d in decisions)
+    low_high = (low, high)
+
+    plt.hist(
+        decisions[0],
+        color="r",
+        alpha=0.5,
+        range=low_high,
+        bins=bins,
+        histtype="stepfilled",
+        density=True,
+        label="Anom (train)",
+    )
+    plt.hist(
+        decisions[1],
+        color="b",
+        alpha=0.5,
+        range=low_high,
+        bins=bins,
+        histtype="stepfilled",
+        density=True,
+        label="Norm (train)",
+    )
+
+    hist, bins = np.histogram(decisions[2], bins=bins, range=low_high, normed=True)
+    scale = len(decisions[2]) / sum(hist)
+    err = np.sqrt(hist * scale) / scale
+
+    width = bins[1] - bins[0]
+    center = (bins[:-1] + bins[1:]) / 2
+    plt.errorbar(center, hist, yerr=err, fmt="o", c="r", label="Anom (test)")
+
+    hist, bins = np.histogram(decisions[3], bins=bins, range=low_high, normed=True)
+    scale = len(decisions[2]) / sum(hist)
+    err = np.sqrt(hist * scale) / scale
+
+    plt.errorbar(center, hist, yerr=err, fmt="o", c="b", label="Normal (test)")
+
+    plt.xlabel("BDT output")
+    plt.ylabel("Arbitrary units")
+    plt.legend(loc="best")
+
+    return fig
 
 
 if __name__ == "__main__":
 
-    df = pd.read_parquet( "/Users/TimothyW/Fun/energy-kaggle/data/prepped_train_features.parquet")
-    
-    features = ['meter_reading','air_temperature','cloud_coverage','dew_temperature','precip_depth_1_hr','sea_level_pressure','wind_direction','wind_speed','hour','weekday','24h_avg','24h_std','z_score']
-    
-    X = df[features]
-    y = df[['anomaly']]
+    df = pd.read_parquet(
+        "/Users/TimothyW/Fun/energy-kaggle/data/prepped_train_features.parquet"
+    )
 
-    group_kf = GroupKFold(n_splits = 3)
+    num_features = [
+        "meter_reading",
+        "air_temperature",
+        "cloud_coverage",
+        "dew_temperature",
+        "precip_depth_1_hr",
+        "sea_level_pressure",
+        "wind_direction",
+        "wind_speed",
+        "24h_avg",
+        "24h_std",
+        "z_score",
+        "square_feet",
+        # "year_built",
+        "floor_count",
+        "air_temperature_mean_lag7",
+        "air_temperature_std_lag7",
+        "minus_24h_val",
+        "minus_week_val",
+        "1w_avg",
+        "1w_std",
+        # "hour",
+        "weekday",
+    ]
+    cat_features = []
+    features = num_features + cat_features
+
+    X = df[features]
+    y = df[["anomaly"]]
+
+    group_kf = GroupKFold(n_splits=3)
     groups = df.building_id.values
 
     clfs = []
-
-    for train_index, test_index in group_kf.split(X,y, groups=groups):    
-        print(train_index)    
+    summaries = []
+    idx = 1
+    for train_index, test_index in group_kf.split(X, y, groups=groups):
         X_train, X_test = X.values[train_index], X.values[test_index]
         y_train, y_test = y.values[train_index], y.values[test_index]
 
-        train_data = Pool(data=X_train,
-                  label=y_train)
+        SandM = SMOTE(random_state=69)
 
-        test_data = Pool(data = X_test, label = y_test)
+        X_train_smote, y_train_smote = SandM.fit_resample(X_train, y_train)
+        print(" SMOTE done, now training ")
 
-        clf = CatBoostClassifier(iterations=100)
+        train_data = Pool(
+            data=X_train_smote,
+            feature_names=features,
+            label=y_train_smote,
+            cat_features=cat_features,
+        )
 
-        clf.fit(train_data, eval_set = test_data, verbose = True)
+        test_data = Pool(
+            data=X_test, feature_names=features, label=y_test, cat_features=cat_features
+        )
+
+        clf = CatBoostClassifier(
+            iterations=1000, early_stopping_rounds=15, eval_metric="AUC"
+        )
+
+        clf.fit(train_data, eval_set=test_data, verbose=True)
 
         clfs.append(clf)
 
+        PLOTS[f"sep_plot_{idx}"] = compare_train_test(
+            clf, X_train, y_train, X_test, y_test
+        )
         predictions = clf.predict(X_test)
 
-        display = RocCurveDisplay.from_predictions(y_test, predictions)
+        print("ROC SCORE = ", roc_auc_score(y_test, predictions))
 
-        #display.plot()
-        #plt.show()
+        fig = plt.figure()
+        ax = fig.gca()
+        ax.cla()
+        RocCurveDisplay.from_predictions(y_test, predictions, ax=ax)
 
-        print(roc_auc_score(y_test, predictions))
+        PLOTS[f"roc_curve_{idx}"] = fig
 
-    #df_test = pd.read_parquet('prepped_test_features.parquet')
+        idx = idx + 1
 
-    #df_submit = create_submission(clfs[1],df_test, features)    
+    for k, v in PLOTS.items():
+        v.savefig(f"{k}.png")
 
+    # df_test = pd.read_parquet("data/prepped_test_features.parquet")
 
+    # df_submit = create_submission(
+    #   clfs[0], df_test, summaries[0]["selected_features_names"]
+    # )
 
-    #check discrete groups + functions
+# df_submit.to_csv("submission_4.csv", index=0)
 
-
-
-        
-
-    
-
-
-
-    
+# FEATURE importances
+# classifier plot
